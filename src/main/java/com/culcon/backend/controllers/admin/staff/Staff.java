9787a2a39241654b0342eeb7c671255fcfd325ea
potasium <potasium.1024@gmail.com>
2024-10-27 08:18:44 +0700

    feat:update exception messages

---
+++ src/main/java/com/culcon/backend/controllers/admin/staff/Staff.java
@@ -0,0 +1,17 @@
+package com.culcon.backend.controllers.admin.staff;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/api/admin/staff")
+public class Staff {
+    @Operation(tags = "Permission Test", summary = "Test permission for admin account")
+    @GetMapping("/test_permission")
+    public String permissionTest() {
+        return "sucess";
+    }
+}
