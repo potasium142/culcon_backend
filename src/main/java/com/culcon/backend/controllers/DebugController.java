5839217fc6b520f951aa011a14cfc444734322b7
potasium <potasium.1024@gmail.com>
2024-11-30 11:59:31 +0700

    feat: add debug controller

--- src/main/java/com/culcon/backend/controllers/DebugController.java
+++ src/main/java/com/culcon/backend/controllers/DebugController.java
@@ -1,16 +1,12 @@
 package com.culcon.backend.controllers;
 
 
-import com.culcon.backend.models.docs.MealKitInfo;
 import com.culcon.backend.models.record.Coupon;
 import com.culcon.backend.models.record.Product;
-import com.culcon.backend.repositories.docs.MealKitInfoDocRepo;
 import com.culcon.backend.repositories.record.CouponRepo;
 import com.culcon.backend.repositories.record.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/debug")
@@ -19,7 +15,6 @@ public class DebugController {
 
 	private final ProductRepo productRepo;
 	private final CouponRepo couponRepo;
-	private final MealKitInfoDocRepo mealKitInfoRepo;
 
 	@GetMapping("/test_permission")
 	public String permissionTest() {
@@ -38,10 +33,5 @@ public class DebugController {
 		@RequestBody Coupon coupon
 	) {
 		return couponRepo.save(coupon);
-	}
-
-	@GetMapping("/docs/mealkit/fetch/all")
-	public List<MealKitInfo> fetchAllProducts() {
-		return mealKitInfoRepo.findAll();
 	}
 }
