2b2325bddc714f9aa175796536bd15e98b79e695
potasium <potasium.1024@gmail.com>
2024-12-27 14:55:08 +0700

    kill me

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.26")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
@@ -48,7 +47,6 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
 
     runtimeOnly("com.h2database:h2")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
 
 //    ===============================
