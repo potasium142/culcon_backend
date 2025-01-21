2361fa9bfd36630b32a31d37665a762fd91bcb4b
potasium <potasium.1024@gmail.com>
2025-01-21 15:45:40 +0700

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,14 +3,6 @@ create extension if not exists vector;
 create sequence order_history_seq
     increment by 50;
 
-create sequence account_otp_id_seq;
-
-alter sequence account_otp_id_seq owned by account_otp.id;
-
-create sequence order_history_id_seq;
-
-alter sequence order_history_id_seq owned by order_history.id;
-
 create type producttype as enum ('VEGETABLE', 'MEAT', 'SEASON', 'MEALKIT');
 
 create type productstatus as enum ('IN_STOCK', 'OUT_OF_STOCK', 'NO_LONGER_IN_SALE');
@@ -26,6 +18,8 @@ create type paymentmethod as enum ('BANKING', 'COD');
 create type paymentstatus as enum ('PENDING', 'RECEIVED', 'REFUNDED', 'REFUNDING', 'CREATED', 'CHANGED');
 
 create type orderstatus as enum ('ON_CONFIRM', 'ON_PROCESSING', 'ON_SHIPPING', 'SHIPPED', 'CANCELLED');
+
+create type commenttype as enum ('POST', 'REPLY');
 
 create table product
 (
@@ -50,6 +44,17 @@ create table staff_account
     type     accounttype   not null,
     status   accountstatus not null,
     token    varchar       not null
+);
+
+create table blog
+(
+    id          varchar(255) not null
+        primary key,
+    title       varchar(255) not null,
+    description text         not null,
+    article     text         not null,
+    thumbnail   varchar(255) not null,
+    infos       jsonb        not null
 );
 
 create table user_account
@@ -91,6 +96,16 @@ create table product_price_history
         primary key (date, product_id)
 );
 
+create table product_embedding
+(
+    id                varchar(255)  not null
+        primary key
+        references product,
+    images_embed_yolo vector(512)[] not null,
+    images_embed_clip vector(768)[] not null,
+    description_embed vector(768)   not null
+);
+
 create table employee_info
 (
     account_id  uuid    not null
@@ -106,6 +121,20 @@ create table employee_info
     dob         date    not null
 );
 
+create table product_doc
+(
+    id                varchar(255)   not null
+        primary key
+        references product,
+    description       varchar(255)   not null,
+    images_url        varchar(255)[] not null,
+    infos             jsonb          not null,
+    ingredients       varchar(255)[],
+    instructions      varchar(255)[],
+    article_md        text           not null,
+    day_before_expiry integer        not null
+);
+
 create table cart
 (
     amount     integer      not null,
@@ -118,12 +147,17 @@ create table cart
 
 create table post_comment
 (
-    timestamp  timestamp    not null,
-    post_id    varchar(255) not null,
-    account_id varchar(255) not null
+    id             varchar(255) not null
+        primary key,
+    timestamp      timestamp    not null,
+    post_id        varchar(255) not null
+        references blog,
+    account_id     varchar(255) not null
         references user_account,
-    comment    varchar(255) not null,
-    primary key (post_id, account_id)
+    parent_comment varchar(255)
+        references post_comment,
+    comment        varchar(255) not null,
+    comment_type   commenttype  not null
 );
 
 create table account_otp
@@ -169,41 +203,6 @@ create table order_history_items
     foreign key (product_id_date, product_id_product_id) references product_price_history
 );
 
-create table product_embedding
-(
-    id                varchar(255)  not null
-        primary key
-        references product,
-    images_embed_yolo vector(512)[] not null,
-    images_embed_clip vector(768)[] not null,
-    description_embed vector(768)   not null
-);
-
-create table product_doc
-(
-    id                varchar(255)   not null
-        primary key
-        references product,
-    description       varchar(255)   not null,
-    images_url        varchar(255)[] not null,
-    infos             jsonb          not null,
-    ingredients       varchar(255)[],
-    instructions      varchar(255)[],
-    article_md        text           not null,
-    day_before_expiry integer        not null
-);
-
-create table blog
-(
-    id          varchar(255) not null
-        primary key,
-    title       varchar(255) not null,
-    description text         not null,
-    article     text         not null,
-    thumbnail   varchar(255) not null,
-    infos       jsonb        not null
-);
-
 create table payment_transaction
 (
     order_id       bigint        not null
@@ -217,3 +216,4 @@ create table payment_transaction
     status         paymentstatus not null,
     amount         real          not null
 );
+
