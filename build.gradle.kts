4d79ba6d63214bb5b0d13ab6e5ffc41992782908
potasium <potasium.1024@gmail.com>
2025-03-13 13:05:14 +0700

    fix test

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,8 +42,9 @@ dependencies {
     implementation("com.cloudinary:cloudinary-http44:1.39.0")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("com.paypal.sdk:paypal-server-sdk:0.6.1")
+
     implementation("com.google.cloud:spring-cloud-gcp-starter")
-    implementation("com.google.cloud:spring-cloud-gcp-starter-sql-postgresql")
+//    runtimeOnly("com.google.cloud:spring-cloud-gcp-starter-sql-postgresql")
 
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
